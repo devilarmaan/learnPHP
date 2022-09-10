564ebf54eda3e96a564426b4c15040686cf84096
devilarmaan <devilarmaan63@gmail.com>
2022-09-11 00:55:48 +0600

    add some file

--- Basic/index.php
+++ Basic/index.php
@@ -3,5 +3,6 @@
 <?php
 
     echo "Hello";
+    print_r([1,2,3]);
 
 ?>
---
+++ Basic/variables.php
@@ -0,0 +1,8 @@
+
+<!-- php data types -->
+
+$name = 'John'; <!--- string---->
+$age = 38; <!--- number---->
+$is_kidss = true; <!--- boolean---->
+
+
