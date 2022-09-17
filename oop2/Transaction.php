36aa8db8ba9f1f83aed3451df940b438af99195d
devilarmaan <devilarmaan63@gmail.com>
2022-09-17 22:00:53 +0600

    some added in oop2

--- oop2/Transaction.php
+++ oop2/Transaction.php
@@ -4,15 +4,31 @@ declare(strict_types=1);
 
 class Transaction
 {
-    public float $amount;
+    private float $amount;
     // private $amount;
-    public string $description;
+    private string $description;
 
     public function __construct(float $amount, string $description)
     {
         $this->amount = $amount;
         $this->description = $description;
     }
+
+    public function addText(float $rate): Transaction // methods chaining
+    {
+        $this->amount += $this->amount * $rate / 10;
+        return $this;
+    }
+
+    private function applyDiscount(float $rate){
+        $this->amount += $this->amount * $rate / 100;
+    }
+
+    public function __destruct()
+    {
+        echo 'Destruct ' . $this->description . '<br />';
+    }
+
 }
 
 
--- oop2/index.php
+++ oop2/index.php
@@ -5,11 +5,15 @@ require_once './Transaction.php';
 
 // classes and object
 
-$transaction = new Transaction(23, 'Transaction1');
+$transaction = (new Transaction(134, 'Transaction1')); // methods chaning
+$transaction->addText(8)->$transaction->amount = 20;
 
-$transaction->amount = 20;
+$transaction2 = (new Transaction(100, 'Transaction2')); // methods chaning
+$transaction2->addText(8)->$transaction2->amount = 20;
 
-var_dump($transaction->amount);
+var_dump($transaction->amount, $transaction2->amount);
+
+unset($transaction);
 
 
 ?>
